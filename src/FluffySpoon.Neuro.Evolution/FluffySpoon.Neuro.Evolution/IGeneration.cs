4ed4f0aedd8a194af9a8fd77f32a3a253f24e86c
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-11-01 17:49:14 +0100

    initial commit.

---
+++ src/FluffySpoon.Neuro.Evolution/FluffySpoon.Neuro.Evolution/IGeneration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffySpoon.Neuro.Evolution
+{
+    public interface IGeneration
+    {
+        IGenome[] Genomes { get; }
+
+        Task<IGeneration> EvolveAsync();
+
+        Task RemoveWorstPerformingGenerations(int amount);
+    }
+}
---
+++ src/FluffySpoon.Neuro.Evolution/FluffySpoon.Neuro.Evolution/IGenome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffySpoon.Neuro.Evolution
+{
+    public interface IGenome
+    {
+        Task AddChainAsync(double[] inputs, double[] outputs);
+
+        Task<IGenome> CrossWithAsync(IGenome other);
+
+        Task MutateAsync();
+    }
+}
